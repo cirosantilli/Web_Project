504d822b85c2cccb5aae3f974d4fd4dcb6510cf1
heng <1924801023@qq.com>
2021-03-06 16:28:46 +0800

    master

---
+++ js/index.js
@@ -0,0 +1,38 @@
+'use strict'
+// function validateForm() {
+//     var x = document.forms["myForm"]["fname"].value;
+//     if (x == null || x == "") {
+//         alert("Please Enter text!");
+//         return false;
+//     }
+// }
+// function myFunction() {
+//     var x, text;
+//     //获取input 的值
+//     x = document.getElementById("numb").value;
+//     //判断
+//     if (isNaN(x) || x < 1 || x > 11) {
+//         text = "输入错误"
+//     }
+//     else {
+//         text = "输入正确"
+//     }
+//     document.getElementById("fuck").innerHTML = text;
+// }
+
+// function foo(x) {
+//     var num = 5;
+//     bar = function foo(y) {
+//         return (x + y + (++num));
+//     }
+// }
+// document.writeln(foo(2));
+// document.writeln(bar(10));
+// document.writeln(bar(10));
+
+/**
+ * author:?
+ * 
+ * date:2020/3/6
+ * 
+ */
---
+++ js/main.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/**
+ *  auther: FUCK
+ *  data:
+ *
+ *
+ *
+ */
+
+// var a = 1;
+//b = 10;
+// let fuck = "cao";
+// var fuck = 12; 
+// console.log(a);
+// var fuck = new Object();
+//var message = "Hello World";
+//if (message) {
+//    alert("value is true");
+//}
+// console.log(Date.now());
+// var d = Date();
+console.log("fuck\ncao");
+var a = 'fuck\ncao';
+for (let i in a) {
+    console.log(a[i]);
+}
+console.log("-------------------");
+// for(let i in document){
+//     console.log(i);
+// }
+for (let i = 0; i < a.length; i++) {
+    console.log(a[i]);
+}
+var NmSl = 119;
+console.log(typeof NmSl.toString());
+function add() {
+    for (let i = 0; i < a.length; i++) {
+        console.log(a[i]);
+    }   
+}
